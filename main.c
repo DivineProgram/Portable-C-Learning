ae411c6e162e90b994a2707e4445d1daf3d4896f
Aden <113607171+DivineProgram@users.noreply.github.com>
2024-10-14 09:49:55 +0000

    Spaced the syntax out a bit

--- main.c
+++ main.c
@@ -10,13 +10,19 @@
 
 int main(void) {
 
+
+  
   // flexFunc(innerFunc);
+
+  
   
   // struct carStock* table[64] = {0};
   // getCarStock(table);
   // printf("%s %s %hd %hd\n", table[1]->price, table[1]->brand, table[1]->year, table[1]->remaining);
   // closeTable(table);
+  
 
+  
   // nonblock(1);
   // printf("!!%c!!\n", getchar());
   // nonblock(0);
